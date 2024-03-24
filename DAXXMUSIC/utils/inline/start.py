74e0c1e20ed90da49f0c2778505680470fc7f5ee
Monster <164752224+Monsterbhai@users.noreply.github.com>
2024-03-25 04:40:34 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["𝐒𝐔𝐌𝐌𝐎𝐍 𝐌𝐄"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
@@ -20,16 +20,16 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text=_["𝐒𝐔𝐌𝐌𝐎𝐍 𝐌𝐄"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["𝐎𝐖𝐍𝐄𝐑"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["𝐂𝐇𝐀𝐍𝐍𝐄𝐋"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["𝐂𝐎𝐌𝐀𝐍𝐃𝐒"], callback_data="settings_back_helper"),
         ],
     ]
     return buttons
