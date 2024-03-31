ef5cc04f4ac6385572c3765980e07e2074152262
Monster <164752224+Monsterbhai@users.noreply.github.com>
2024-03-31 09:48:54 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -20,8 +20,7 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["𝐒𝐔𝐌𝐌𝐎𝐍𝐌𝐄"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["𝐒𝐔𝐌𝐌𝐎𝐍 𝐌𝐄"], url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
