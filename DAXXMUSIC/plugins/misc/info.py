8b27a3f5b6007e3d92ef0f023f02da3452281247
Anuj1154 <158982341+Anuj1154@users.noreply.github.com>
2024-04-25 10:23:27 +0530

    Update info.py

--- DAXXMUSIC/plugins/misc/info.py
+++ DAXXMUSIC/plugins/misc/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/mannu_music_bot?startgroup=true"),
     ],
 ]
 
@@ -73,7 +73,7 @@ INFO_TEXT = """
 ๏ ᴜsᴇʀ ᴅᴄ ɪᴅ ➠ {}
 ๏ ᴜsᴇʀ ʙɪᴏ ➠ {}
 
-๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [๛ɴ ʏ ᴋ ᴀ ᴀ࿐](https://t.me/nykaaxbot)
+๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [๛𝗠𝗔𝗡𝗡𝗨𝗠𝗨𝗦𝗜𝗖࿐](https://t.me/mannu_music_bot)
 """
 
 # --------------------------------------------------------------------------------- #
